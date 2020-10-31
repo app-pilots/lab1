4650658b15d3a7209717d063971ac29f9037c9a6
lopez damian <dennyslopez2013@gmail.com>
2020-10-30 22:51:35 -0500

    init

---
+++ app/src/main/java/com/apps/mediconline/MainActivity.java
@@ -0,0 +1,25 @@
+package com.apps.mediconline;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(intent);
+        finish();
+
+
+
+
+    }
+}
